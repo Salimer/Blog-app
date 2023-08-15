5f81b15cbb010883f2065c2ba4003faa9392f691
Salimer <52242629+Salimer@users.noreply.github.com>
2023-08-15 15:27:03 +0300

    Update 20230815094031_create_comments.rb

--- db/migrate/20230815094031_create_comments.rb
+++ db/migrate/20230815094031_create_comments.rb
@@ -1,8 +1,8 @@
 class CreateComments < ActiveRecord::Migration[7.0]
   def change
     create_table :comments do |t|
-      t.references :author_id, foreign_key: { to_table: :users }
-      t.references :post_id, foreign_key: { to_table: :posts }
+      t.references :author, foreign_key: { to_table: :users }
+      t.references :post, foreign_key: { to_table: :posts }
       t.text :text
 
       t.timestamps
