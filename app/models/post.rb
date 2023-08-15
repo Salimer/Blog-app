ef04ed64dbf7fc5f1e1e0a83ecda31462c50c5fd
Salimer <52242629+Salimer@users.noreply.github.com>
2023-08-15 19:55:34 +0300

    Create post.rb

---
+++ app/models/post.rb
@@ -0,0 +1,21 @@
+class Post < ApplicationRecord
+    # Associations
+    belongs_to :author, class_name: 'User'
+    has_many :comments
+    has_many :likes
+
+    # Attributes
+    attribute :title, :string
+    attribute :text, :text
+    attribute :comments_counter, :integer, default: 0
+    attribute :likes_counter, :integer, default: 0
+
+    # Methods
+    def update_user_posts_counter
+        author.update(posts_counter: author.posts.count)
+    end
+
+    def five_most_recent_comments
+        comments.order(created_at: :desc).limit(5)
+    end
+end
