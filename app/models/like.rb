19a4e550e8a09f85d18465b7d235812ea77361ea
Salimer <52242629+Salimer@users.noreply.github.com>
2023-08-15 21:34:54 +0300

    Update like.rb

--- app/models/like.rb
+++ app/models/like.rb
@@ -3,6 +3,9 @@ class Like < ApplicationRecord
   belongs_to :author, class_name: 'User'
   belongs_to :post, class_name: 'Post'
 
+  # Callbacks
+  after_save :update_post_likes_counter
+
   # Methods
   def update_post_likes_counter
     post.update(likes_counter: post.likes.count)
