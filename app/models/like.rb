39175f572ffdecf11ad81f19e679680f308efead
Salimer <52242629+Salimer@users.noreply.github.com>
2023-08-15 19:55:35 +0300

    Create like.rb

---
+++ app/models/like.rb
@@ -0,0 +1,10 @@
+class Like < ApplicationRecord
+    # Associations
+    belongs_to :author, class_name: 'User'
+    belongs_to :post, class_name: 'Post'
+
+    # Methods
+    def update_post_likes_counter
+        post.update(likes_counter: post.likes.count)
+    end
+end
