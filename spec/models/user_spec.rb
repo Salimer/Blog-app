73d7c59fc215798e5eb6cf5e0bf24a2a29e1b6c1
Salimer <52242629+Salimer@users.noreply.github.com>
2023-08-16 17:22:58 +0300

    Update user_spec.rb

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -10,5 +10,10 @@ RSpec.describe User, type: :model do
       subject.name = nil
       expect(subject).to_not be_valid
     end
+
+    it 'posts_counter should be integer' do
+      subject.posts_counter = 'hey'
+      expect(subject).to_not be_valid
+    end
   end
 end
