a94be440060de64238eb97ee4ab7793f6929f75a
Salimer <52242629+Salimer@users.noreply.github.com>
2023-08-16 17:21:52 +0300

    Update post_spec.rb

--- spec/models/post_spec.rb
+++ spec/models/post_spec.rb
@@ -9,4 +9,13 @@ RSpec.describe Post, type: :model do
       subject.title = nil
       expect(subject).to_not be_valid
     end
+
+    it 'title should be less than 251 chars' do
+      subject.title = 'Lorem ipsum dolor sit amet,
+      consectetuer adipiscing elit. Aenean commodo ligula
+      eget dolor. Aenean massa. Cum sociis natoque penatibus
+      et magnis dis parturient montes, nascetur ridiculus mus.
+      Donec quam felis, ultricies neln ea' # 251 chars
+      expect(subject).to_not be_valid
+    end
 end
