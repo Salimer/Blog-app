132bea5164a3163982eb0ebad3c1cca8f11325e5
Salimer <52242629+Salimer@users.noreply.github.com>
2023-08-21 13:32:11 +0300

    Update users_controller.rb

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,6 @@
 class UsersController < ApplicationController
+    before_action :find_user, only: [:show]
+
     def index
         @users = User.all
     end
@@ -9,5 +11,13 @@ class UsersController < ApplicationController
 
     def new
 
+    end
+
+    def find_user
+        @user = User.find(params[:id])
+        rescue ActiveRecord::RecordNotFound
+        # Handle the situation when the post is not found
+        flash[:alert] = 'User not found, back to users page'
+        redirect_to users_path()
     end
 end
