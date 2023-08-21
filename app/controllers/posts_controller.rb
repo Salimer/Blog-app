86fbb2e07d1d1cc26d9e66c38909187466da83d3
Salimer <52242629+Salimer@users.noreply.github.com>
2023-08-21 13:31:52 +0300

    Update posts_controller.rb

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,4 +1,7 @@
 class PostsController < ApplicationController
+    before_action :find_user, only: [:index, :show]
+  before_action :find_post, only: [:show]
+
     def index
         @posts = @user.posts
     end
@@ -11,4 +14,15 @@ class PostsController < ApplicationController
 
     end
 
+    def find_user
+        @user = User.find(params[:user_id])
+    end
+
+    def find_post
+        @post = @user.posts.find(params[:id])
+      rescue ActiveRecord::RecordNotFound
+        # Handle the situation when the post is not found
+        flash[:alert] = 'Post not found, back to posts page'
+        redirect_to user_posts_path(@user)
+    end
 end
