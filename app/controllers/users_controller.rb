081f2a6c736f0923a4ec62cf97f88c51445f77f7
Salimer <52242629+Salimer@users.noreply.github.com>
2023-08-21 13:34:28 +0300

    Fix linters

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,23 +1,19 @@
 class UsersController < ApplicationController
-    before_action :find_user, only: [:show]
+  before_action :find_user, only: [:show]
 
-    def index
-        @users = User.all
-    end
+  def index
+    @users = User.all
+  end
 
-    def show
-        
-    end
+  def show; end
 
-    def new
+  def new; end
 
-    end
-
-    def find_user
-        @user = User.find(params[:id])
-        rescue ActiveRecord::RecordNotFound
-        # Handle the situation when the post is not found
-        flash[:alert] = 'User not found, back to users page'
-        redirect_to users_path()
-    end
+  def find_user
+    @user = User.find(params[:id])
+  rescue ActiveRecord::RecordNotFound
+    # Handle the situation when the post is not found
+    flash[:alert] = 'User not found, back to users page'
+    redirect_to users_path
+  end
 end
