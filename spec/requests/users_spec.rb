a296884c5f4a6427cab4bb17a85754faab0a9804
Salimer <52242629+Salimer@users.noreply.github.com>
2023-08-22 11:44:38 +0300

    Update users_spec.rb

--- spec/requests/users_spec.rb
+++ spec/requests/users_spec.rb
@@ -12,6 +12,9 @@ RSpec.describe 'UsersController', type: :request do
       it 'response status is correct' do
         expect(response).to have_http_status(200)
       end
+      it 'correct template is rendered' do
+        expect(response).to render_template(:index)
+      end
   end
   end
 end
