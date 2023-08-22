9ec6e680c22ede8eb2495c5c551f179430fa86b1
Salimer <52242629+Salimer@users.noreply.github.com>
2023-08-22 11:44:19 +0300

    Update users_spec.rb

--- spec/requests/users_spec.rb
+++ spec/requests/users_spec.rb
@@ -1,6 +1,8 @@
 require 'rails_helper'
 
-RSpec.describe 'Users', type: :request do
+RSpec.describe 'UsersController', type: :request do
+  let(:user) { User.create(name: 'Test User') }
+
   describe 'GET /index' do
     pending "add some examples (or delete) #{__FILE__}"
   end
