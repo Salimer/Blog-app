20c255be9665efee88b93105e04c07434dd8818e
Salimer <52242629+Salimer@users.noreply.github.com>
2023-08-22 11:43:15 +0300

    Update posts_spec.rb

--- spec/requests/posts_spec.rb
+++ spec/requests/posts_spec.rb
@@ -1,6 +1,9 @@
 require 'rails_helper'
 
-RSpec.describe 'Posts', type: :request do
+RSpec.describe 'PostsController', type: :request do
+  let(:user) { User.create(name: 'Test User') }
+  let(:post) { Post.create(author: user, title: 'Test Post') }
+
   describe 'GET /index' do
     pending "add some examples (or delete) #{__FILE__}"
   end
