48f6efcb6efbf2c8dc9d3d367991ccb9d40196d5
Salimer <52242629+Salimer@users.noreply.github.com>
2023-08-23 21:33:37 +0300

    Update comments_controller.rb

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -6,4 +6,15 @@ class CommentsController < ApplicationController
         @comment = @post.comments.new
     end
 
+    def create
+        @comment = @post.comments.new(comment_params)
+        @comment.author = @user # Assign the current user to the comment
+        if @comment.save
+            flash[:notice] = 'Comment created successfully.'
+            redirect_to user_post_path(@user, @post)
+        else
+            render 'new'
+        end
+    end
+
 end
