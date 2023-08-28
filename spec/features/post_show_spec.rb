9e0c1ad3dca1d6f01bd860483ba50e48936b3e87
Salimer <52242629+Salimer@users.noreply.github.com>
2023-08-28 19:04:41 +0300

    Refactor post_show_spec.rb

--- spec/features/post_show_spec.rb
+++ spec/features/post_show_spec.rb
@@ -1,106 +1,38 @@
 require 'rails_helper'
 
-RSpec.feature "Post Show", type: :feature do
-  scenario 'see the title of the post' do
-    user = User.create(name: 'Tom')
-    post = Post.create(author: user, title: "first post's title")
-
+RSpec.feature 'Post Show', type: :feature do
+  let(:user) { User.create(name: 'Tom', photo: 'https://www.kasandbox.org/programming-images/avatars/leaf-blue.png', bio: 'He is a good programmar') }
+  let!(:post) { Post.create(author: user, title: "first post's title", text: 'first text') }
+  let!(:post2) { Post.create(author: user, title: 'second post', text: 'second text') }
+  let!(:post3) { Post.create(author: user, title: 'third post', text: '3 text') }
+  let!(:post4) { Post.create(author: user, title: '4 post', text: '4 text') }
+  let!(:comment1) { Comment.create(author: user, post:, text: 'first comment') }
+  let!(:comment2) { Comment.create(author: user, post:, text: 'second comment') }
+  let!(:comment3) { Comment.create(author: user, post:, text: 'third comment') }
+  let!(:like1) { Like.create(author: user, post:) }
+  scenario 'see the title of the post and who wrote it and the interactions' do
     visit user_post_path(user, post)
-
     expect(page).to have_content("first post's title")
-  end
-
-  scenario 'see who wrote the post' do
-    user = User.create(name: 'Tom')
-    post = Post.create(author: user, title: "first post's title")
-
-    visit user_post_path(user, post)
-
-    expect(page).to have_content("by Tom")
-  end
-
-  scenario 'see how many comments a post has' do
-    user = User.create(name: 'Tom')
-    post = Post.create(author: user, title: "first post's title")
-    comment1 = Comment.create(author: user, post: post, text: 'first comment')
-    comment2 = Comment.create(author: user, post: post, text: 'second comment')
-    comment3 = Comment.create(author: user, post: post, text: 'third comment')
-    comment4 = Comment.create(author: user, post: post, text: 'fourth comment')
-    comment5 = Comment.create(author: user, post: post, text: 'fifth comment')
-    comment6 = Comment.create(author: user, post: post, text: 'sixth comment')
-
-    visit user_post_path(user, post)
-
-    expect(page).to have_content("Comments: 6")
-  end
-
-  scenario 'see how many likes a post has' do
-    user = User.create(name: 'Tom')
-    post = Post.create(author: user, title: "first post's title")
-    like1 = Like.create(author: user, post: post)
-    like2 = Like.create(author: user, post: post)
-    like3 = Like.create(author: user, post: post)
-    like4 = Like.create(author: user, post: post)
-    like5 = Like.create(author: user, post: post)
-    like6 = Like.create(author: user, post: post)
-
-    visit user_post_path(user, post)
-
-    expect(page).to have_content("Likes: 6")
+    expect(page).to have_content('by Tom')
+    expect(page).to have_content('Comments: 3')
+    expect(page).to have_content('Likes: 1')
   end
 
   scenario "see the post's body" do
-    body = "Lorem Ipsum is simply dummy text of the printing and typesetting
-    industry. Lorem Ipsum has been the industry's standard dummy text ever since
-    the 1500s, when an unknown printer took a galley of type and scrambled it to
-    make a type specimen book. It has survived not only five centuries, but also
-    the leap into electronic typesetting, remaining essentially unchanged.
-    It was popularised in the 1960s with the release of Letraset sheets containing
-    Lorem Ipsum passages, and more recently with desktop publishing software like
-    Aldus PageMaker including versions of Lorem Ipsum."
-
-    user = User.create(name: 'Tom')
-    post1 = Post.create(author: user, title: "first post's title", text: body)
-
-    visit user_posts_path(user)
-
-    expect(page).not_to have_content(body)
+    visit user_post_path(user, post)
+    expect(page).to have_content('first text')
   end
 
-  scenario 'see the username of each commentor' do
-    user = User.create(name: 'Tom')
+  scenario 'see the username and comment of each post' do
     user2 = User.create(name: 'Ali')
     user3 = User.create(name: 'Salim')
-    post = Post.create(author: user, title: "first post's title")
-    comment1 = Comment.create(author: user, post: post, text: 'first comment')
-    comment2 = Comment.create(author: user, post: post, text: 'second comment')
-    comment3 = Comment.create(author: user, post: post, text: 'third comment')
-    comment4 = Comment.create(author: user, post: post, text: 'fourth comment')
-    comment5 = Comment.create(author: user2, post: post, text: 'fifth comment')
-    comment6 = Comment.create(author: user3, post: post, text: 'sixth comment')
+    Comment.create(author: user2, post:, text: 'fifth comment')
+    Comment.create(author: user3, post:, text: 'sixth comment')
 
     visit user_post_path(user, post)
 
-    expect(page).to have_content("Tom")
-    expect(page).to have_content("Ali")
-    expect(page).to have_content("Salim")
-  end
-
-  scenario 'see the username of each commentor' do
-    user = User.create(name: 'Tom')
-    user2 = User.create(name: 'Ali')
-    user3 = User.create(name: 'Salim')
-    post = Post.create(author: user, title: "first post's title")
-    comment1 = Comment.create(author: user, post: post, text: 'first comment')
-    comment2 = Comment.create(author: user, post: post, text: 'second comment')
-    comment3 = Comment.create(author: user3, post: post, text: 'third comment')
-    comment4 = Comment.create(author: user, post: post, text: 'fourth comment')
-    comment5 = Comment.create(author: user2, post: post, text: 'fifth comment')
-    comment6 = Comment.create(author: user, post: post, text: 'last comment')
-
-    visit user_post_path(user, post)
-
-    expect(page).to have_content("first comment")
-    expect(page).to have_content("last comment")
+    expect(page).to have_content('Tom')
+    expect(page).to have_content('Salim')
+    expect(page).to have_content('sixth comment')
   end
 end
