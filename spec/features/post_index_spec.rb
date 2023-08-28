d45788eb936610911991e32da4607b6dacfa8978
Salimer <52242629+Salimer@users.noreply.github.com>
2023-08-28 17:52:06 +0300

    Update post_index_spec.rb

--- spec/features/post_index_spec.rb
+++ spec/features/post_index_spec.rb
@@ -30,4 +30,23 @@ RSpec.feature "Post Index", type: :feature do
     expect(page).to have_content("first post's title")
   end
 
+  scenario "see some of the post's body" do
+    body = "Lorem Ipsum is simply dummy text of the printing and typesetting
+    industry. Lorem Ipsum has been the industry's standard dummy text ever since
+    the 1500s, when an unknown printer took a galley of type and scrambled it to
+    make a type specimen book. It has survived not only five centuries, but also
+    the leap into electronic typesetting, remaining essentially unchanged.
+    It was popularised in the 1960s with the release of Letraset sheets containing
+    Lorem Ipsum passages, and more recently with desktop publishing software like
+    Aldus PageMaker including versions of Lorem Ipsum."
+
+    user = User.create(name: 'Tom')
+    post1 = Post.create(author: user, title: "first post's title", text: body)
+
+    visit user_posts_path(user)
+
+    expect(page).to have_content("Lorem Ipsum is simply dummy text of the printing")
+    expect(page).not_to have_content(body)
+  end
+
 end
