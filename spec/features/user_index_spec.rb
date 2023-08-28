148db165cfb14d1c51259dce0d52887277140bd0
Alishah Safdari <alishahsafdari905@gmail.com>
2023-08-28 17:26:17 +0430

    Update user_index file

--- spec/features/user_index_spec.rb
+++ spec/features/user_index_spec.rb
@@ -9,5 +9,17 @@ RSpec.feature "User Index", type: :feature do
 
     expect(page).to have_content("Tom")
     expect(page).to have_content("Ali")
+    expect(page).to have_css("img[alt='Tom'], count: 1")
+    expect(page).to have_css("img[alt='Ali'], count: 1")
+  end
+
+  scenario 'visiting the user index page, you see the number of posts each user has written..' do
+    user1 = User.create(name: 'Tom', posts_counter: 2 )
+    user2 = User.create(name: 'Ali', posts_counter: 1 )
+
+    visit users_path
+
+    expect(page).to have_content("2")
+    expect(page).to have_content("1")
   end
 end
