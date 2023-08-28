9fa3a92a807dc2db62142915d9ce7df289cf8d77
Salimer <52242629+Salimer@users.noreply.github.com>
2023-08-28 14:36:15 +0300

    Update comment.rb

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -11,7 +11,11 @@ class Comment < ApplicationRecord
   after_destroy :decrement_post_comments_counter
 
   # Methods
-  def update_post_comments_counter
-    post.update(comments_counter: post.comments.count)
+  def increase_post_comments_counter
+    post.increment!(:comments_counter)
+  end
+
+  def decrement_post_comments_counter
+    post.decrement!(:comments_counter)
   end
 end
