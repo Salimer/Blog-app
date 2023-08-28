67503e5520ef6810acdbe0b9a0bc21944d9deef4
Salimer <52242629+Salimer@users.noreply.github.com>
2023-08-28 17:52:51 +0300

    Update post_show_spec.rb

--- spec/features/post_show_spec.rb
+++ spec/features/post_show_spec.rb
@@ -19,4 +19,19 @@ RSpec.feature "Post Show", type: :feature do
     expect(page).to have_content("by Tom")
   end
 
+  scenario 'see how many comments a post has' do
+    user = User.create(name: 'Tom')
+    post = Post.create(author: user, title: "first post's title")
+    comment1 = Comment.create(author: user, post: post, text: 'first comment')
+    comment2 = Comment.create(author: user, post: post, text: 'second comment')
+    comment3 = Comment.create(author: user, post: post, text: 'third comment')
+    comment4 = Comment.create(author: user, post: post, text: 'fourth comment')
+    comment5 = Comment.create(author: user, post: post, text: 'fifth comment')
+    comment6 = Comment.create(author: user, post: post, text: 'sixth comment')
+
+    visit user_post_path(user, post)
+
+    expect(page).to have_content("Comments: 6")
+  end
+
 end
