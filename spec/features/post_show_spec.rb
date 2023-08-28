54ed88e3fe1c7714fb5d438284a61f76a4768f2a
Salimer <52242629+Salimer@users.noreply.github.com>
2023-08-28 17:52:43 +0300

    Update post_show_spec.rb

--- spec/features/post_show_spec.rb
+++ spec/features/post_show_spec.rb
@@ -1,4 +1,13 @@
 require 'rails_helper'
 
 RSpec.feature "Post Show", type: :feature do
+  scenario 'see the title of the post' do
+    user = User.create(name: 'Tom')
+    post = Post.create(author: user, title: "first post's title")
+
+    visit user_post_path(user, post)
+
+    expect(page).to have_content("first post's title")
+  end
+
 end
