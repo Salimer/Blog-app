dcfab3e26d32fbe3be95959d77478dc66477aca4
Salimer <52242629+Salimer@users.noreply.github.com>
2023-08-28 17:51:55 +0300

    Update post_index_spec.rb

--- spec/features/post_index_spec.rb
+++ spec/features/post_index_spec.rb
@@ -1,3 +1,12 @@
 require 'rails_helper'
+
 RSpec.feature "Post Index", type: :feature do
+  scenario "see user's profile picture and username" do
+    user = User.create(name: 'Tom', photo: 'https://www.kasandbox.org/programming-images/avatars/leaf-blue.png')
+
+    visit user_posts_path(user)
+
+    expect(page).to have_content("Tom")
+    expect(page).to have_css("img[alt='Tom']", count: 1)
+  end
 end
