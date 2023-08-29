4d5a297163caa85c7067e70055da6b011504d408
Salimer <52242629+Salimer@users.noreply.github.com>
2023-08-30 01:31:20 +0300

    Add custom route, controller and view to make sign_out work

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,11 @@
 Rails.application.routes.draw do
+  devise_for :users
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
   # root "articles#index"
+  get '/sign_out', to: 'custom_sessions#destroy', as: :custom_sign_out
+
   resources :users do
     resources :posts do
       member do
