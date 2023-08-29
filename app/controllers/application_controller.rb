ccde2875edeacb86de1adb186166ca1a3182326b
Salimer <52242629+Salimer@users.noreply.github.com>
2023-08-30 01:26:39 +0300

    Remove current_user method

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,9 +1,6 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
 
-  def current_user
-    @current_user ||= User.first
-  end
 
   helper_method :current_user
 end
