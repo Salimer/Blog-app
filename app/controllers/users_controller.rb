e77adf13327f492b3054acf5daf02d77c841c382
Salimer <52242629+Salimer@users.noreply.github.com>
2023-08-30 01:30:45 +0300

    Update users_controller.rb

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,5 @@
 class UsersController < ApplicationController
+  before_action :authenticate_user!, only: [:show]
   before_action :find_user, only: [:show]
 
   def index
@@ -6,13 +7,17 @@ class UsersController < ApplicationController
   end
 
   def show
-    @three_recent_posts = @user.three_most_recent_posts
+    if @user.nil?
+      @users = User.includes(:posts).all
+      render 'index'
+    end
+    @three_recent_posts = @user.three_most_recent_posts unless @user.nil?
   end
 
   def new; end
 
   def find_user
-    @user = User.find(params[:id])
+    @user = User.find(params[:id]) unless User.exists?
   rescue ActiveRecord::RecordNotFound
     # Handle the situation when the post is not found
     flash[:alert] = 'User not found, back to users page'
