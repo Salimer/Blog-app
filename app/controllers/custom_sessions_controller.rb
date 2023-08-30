63c5c2e334016abcd7f12009aebd343d4387958c
Alishah Safdari <alishahsafdari905@gmail.com>
2023-08-30 11:38:04 +0430

    Update custom_sessions_controller

--- app/controllers/custom_sessions_controller.rb
+++ app/controllers/custom_sessions_controller.rb
@@ -1,8 +1,8 @@
 class CustomSessionsController < ApplicationController
-    def destroy
-      # Implement your sign-out logic here, such as clearing session data.
-      sign_out(current_user) if user_signed_in?
-      redirect_to destroy_user_session_path, notice: 'You have been signed out.'
-    end
+  def destroy
+    # Implement your sign-out logic here, such as clearing session data.
+    sign_out(current_user) if user_signed_in?
+    redirect_to destroy_user_session_path, notice: 'You have been signed out.'
   end
+end
   
