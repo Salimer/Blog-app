6e63adb5d91d7c950ab3fa8b9e005d1d03bb996b
Salimer <52242629+Salimer@users.noreply.github.com>
2023-08-30 11:38:04 +0300

    Remove custom setup for sign out

--- app/controllers/custom_sessions_controller.rb
+++
@@ -1,7 +0,0 @@
-class CustomSessionsController < ApplicationController
-  def destroy
-    # Implement your sign-out logic here, such as clearing session data.
-    sign_out(current_user) if user_signed_in?
-    redirect_to destroy_user_session_path, notice: 'You have been signed out.'
-  end
-end
--- config/routes.rb
+++ config/routes.rb
@@ -3,8 +3,7 @@ Rails.application.routes.draw do
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
-  # root "articles#index"
-  get '/sign_out', to: 'custom_sessions#destroy', as: :custom_sign_out
+  root 'users#index'
 
   resources :users do
     resources :posts do
