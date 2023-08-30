80d9d41aa5582f8f71789f0a81e6a1bbf3baa49b
Salimer <52242629+Salimer@users.noreply.github.com>
2023-08-30 13:42:11 +0300

    Update development.rb

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -41,8 +41,6 @@ Rails.application.configure do
 
   config.action_mailer.perform_caching = false
 
-  config.action_mailer.default_url_options = { host: 'localhost', port: 3000 }
-
   # Print deprecation notices to the Rails logger.
   config.active_support.deprecation = :log
 
@@ -69,4 +67,5 @@ Rails.application.configure do
 
   # Uncomment if you wish to allow Action Cable access from any origin.
   # config.action_cable.disable_request_forgery_protection = true
+  config.action_mailer.default_url_options = { host: "localhost", port: 3000 }
 end
