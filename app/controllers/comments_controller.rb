b3b889f5fbfaa22fe37455c9b6e905eb23919c9c
Salimer <52242629+Salimer@users.noreply.github.com>
2023-08-30 19:44:45 +0300

    Update comments_controller.rb

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,6 +1,7 @@
 class CommentsController < ApplicationController
   before_action :find_user
   before_action :find_post
+  before_action :find_comment, only: :destroy
 
   def new
     @comment = @post.comments.new
@@ -11,10 +12,22 @@ class CommentsController < ApplicationController
     @comment.author = @user # Assign the current user to the comment
     if @comment.save
       flash[:notice] = 'Comment created successfully.'
-      redirect_to user_post_path(@user, @post)
+      redirect_to user_post_path(@post.author, @post)
     else
       render 'new'
     end
+  end
+
+  def destroy
+    authorize! :delete, @comment
+
+    if @comment.destroy
+      flash[:notice] = 'Comment was successfully deleted.'
+    else
+      flash[:alert] = 'Failed to delete comment.'
+    end
+
+    redirect_to user_post_path(@user, @post)
   end
 
   private
@@ -25,6 +38,10 @@ class CommentsController < ApplicationController
 
   def find_post
     @post = Post.find(params[:post_id])
+  end
+
+  def find_comment
+    @comment = @post.comments.find(params[:id])
   end
 
   def comment_params
