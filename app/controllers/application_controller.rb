55cf9c5816a26ac83df6f62c06cc4cff09b5892b
Salimer <52242629+Salimer@users.noreply.github.com>
2023-08-30 19:44:47 +0300

    Update application_controller.rb

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,7 +3,7 @@ class ApplicationController < ActionController::Base
 
   before_action :configure_permitted_parameters, if: :devise_controller?
   def configure_permitted_parameters
-    devise_parameter_sanitizer.permit(:sign_up, keys: [:name])
+    devise_parameter_sanitizer.permit(:sign_up, keys: [:name, :role])
   end
 
   def configure_devise_parameters
