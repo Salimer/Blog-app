4678f5a3761be8eded3a606f17c5cdea4c395800
Salimer <52242629+Salimer@users.noreply.github.com>
2023-08-31 11:55:09 +0300

    Update comments_controller.rb

--- app/controllers/api/v1/comments_controller.rb
+++ app/controllers/api/v1/comments_controller.rb
@@ -6,6 +6,17 @@ class Api::V1::CommentsController < ActionController::API
       render json: @comments
     end
 
+    def create
+        @comment = @post.comments.new(comment_params)
+        @comment.author = @user
+
+        if @comment.save
+            render json: @comment, status: :created
+        else
+            render json: { error: @comment.errors.full_messages }, status: :unprocessable_entity
+        end
+    end
+  
     def find_user_and_post
       @user = User.find(params[:user_id])
       @post = @user.posts.find(params[:post_id])
