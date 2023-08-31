dff06b4a59c28e2a9f206906c1a011af5f957a33
Salimer <52242629+Salimer@users.noreply.github.com>
2023-08-31 11:56:13 +0300

    Update posts_controller.rb

--- app/controllers/api/v1/posts_controller.rb
+++ app/controllers/api/v1/posts_controller.rb
@@ -1,3 +1,9 @@
 class Api::V1::PostsController < ActionController::API
+    before_action :find_user
+    private
+  
+    def find_user
+      @user = User.find(params[:user_id])
+    end
   end
   
