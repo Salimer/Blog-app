09ee94646f5997cdaa9fde277186a78046b7576a
Salimer <52242629+Salimer@users.noreply.github.com>
2023-08-31 12:43:05 +0300

    Limit routes

--- config/routes.rb
+++ config/routes.rb
@@ -17,8 +17,8 @@ Rails.application.routes.draw do
 
   namespace :api do
     namespace :v1 do
-      resources :users do
-        resources :posts do
+      resources :users, only: [:index] do
+        resources :posts, only: [:index] do
           resources :comments, only: [:index, :create]
         end
       end
