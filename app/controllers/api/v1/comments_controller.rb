d350016f9c8bc83dcb46ea9217f6d930cab096ff
Salimer <52242629+Salimer@users.noreply.github.com>
2023-08-31 12:23:49 +0300

    Fix linters

--- app/controllers/api/v1/comments_controller.rb
+++ app/controllers/api/v1/comments_controller.rb
@@ -1,30 +1,30 @@
 class Api::V1::CommentsController < ActionController::API
-    before_action :find_user_and_post
-  
-    def index
-      @comments = @post.comments
-      render json: @comments
-    end
+  before_action :find_user_and_post
 
-    def create
-        @comment = @post.comments.new(comment_params)
-        @comment.author = @user
+  def index
+    @comments = @post.comments
+    render json: @comments
+  end
 
-        if @comment.save
-            render json: @comment, status: :created
-        else
-            render json: { error: @comment.errors.full_messages }, status: :unprocessable_entity
-        end
-    end
-  
-    private
+  def create
+    @comment = @post.comments.new(comment_params)
+    @comment.author = @user
 
-    def comment_params
-        params.require(:comment).permit(:text)
+    if @comment.save
+      render json: @comment, status: :created
+    else
+      render json: { error: @comment.errors.full_messages }, status: :unprocessable_entity
     end
-  
-    def find_user_and_post
-      @user = User.find(params[:user_id])
-      @post = @user.posts.find(params[:post_id])
-    end
+  end
+
+  private
+
+  def comment_params
+    params.require(:comment).permit(:text)
+  end
+
+  def find_user_and_post
+    @user = User.find(params[:user_id])
+    @post = @user.posts.find(params[:post_id])
+  end
 end
