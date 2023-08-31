d2c2ca7aa97fce0438fe4c6945b67fb20d35f8fa
Salimer <52242629+Salimer@users.noreply.github.com>
2023-08-31 11:54:42 +0300

    Update comments_controller.rb

--- app/controllers/api/v1/comments_controller.rb
+++ app/controllers/api/v1/comments_controller.rb
@@ -1,3 +1,8 @@
 class Api::V1::CommentsController < ActionController::API
+    before_action :find_user_and_post
+    def find_user_and_post
+      @user = User.find(params[:user_id])
+      @post = @user.posts.find(params[:post_id])
+    end
   end
   
