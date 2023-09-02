a13fb731ef1ff4cbd7d2a88bc32d0701c17b64eb
Salimer <52242629+Salimer@users.noreply.github.com>
2023-09-02 11:13:05 +0300

    Update posts_spec.rb

--- spec/requests/api/v1/posts_spec.rb
+++ spec/requests/api/v1/posts_spec.rb
@@ -1,7 +1,6 @@
 require 'swagger_helper'
 
 RSpec.describe 'api/v1/posts', type: :request do
-
   path '/api/v1/users/{user_id}/posts' do
     # You'll want to customize the parameter types...
     parameter name: 'user_id', in: :path, type: :string, description: 'user_id'
